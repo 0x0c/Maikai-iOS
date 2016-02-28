aade434a63579a43b0f55f29255854c8bcda9ae2
Akira Matsuda <akira.matsuda@me.com>
2016-02-28 17:46:35 +0900

    Initial Commit

---
+++ Maikai/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Maikai
+//
+//  Created by Akira Matsuda on 2/28/16.
+//  Copyright © 2016 Akira Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
